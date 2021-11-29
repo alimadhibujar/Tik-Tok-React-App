4fa0ebee1c9711c3343efc825fe182dce6455efe
Bujar Alimadhi <b.alimadhi@gmail.com>
2021-11-29 12:29:49 +0100

    adding app phone cover

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,8 @@ function App() {
     {
       url: "https://res.cloudinary.com/dwpp1insg/video/upload/v1636470265/tik-tok/Kemisha_shkon_shume_ildpgv.mp4",
       channel: "Membersi",
-      description: "#Atletet 25, #pantallonat 15 😂, këmisha shkon, po shkon 30 e ca 😂",
+      description:
+        "#Atletet 25, #pantallonat 15 😂, këmisha shkon, po shkon 30 e ca 😂",
       song: "Bounce - Ruger",
       likes: 12400,
       messages: 163,
@@ -16,7 +17,8 @@ function App() {
     {
       url: "https://res.cloudinary.com/dwpp1insg/video/upload/v1636471056/tik-tok/ola_ola023_-_6990027201517227270_daxgfm.mp4",
       channel: "ola_ola023",
-      description: "Po çfarë jete po bën ti? # çantën #në krahë #kokrrën e qefit 😂 u bën votimet unë vizhguse 😂 videos on TikTok",
+      description:
+        "Po çfarë jete po bën ti? # çantën #në krahë #kokrrën e qefit 😂 u bën votimet unë vizhguse 😂 videos on TikTok",
       song: "",
       likes: 17500,
       messages: 320,
@@ -25,7 +27,8 @@ function App() {
     {
       url: "https://res.cloudinary.com/dwpp1insg/video/upload/v1636536955/tik-tok/Can_yours_do_that_boyya1.mp4",
       channel: "coco224466",
-      description: "Can yours do that?😡😛🥺🤪#pet #dogs #fouyou #animals #cute #funnyvideo #fy #dogsoftiktok #puppy #poodle",
+      description:
+        "Can yours do that?😡😛🥺🤪#pet #dogs #fouyou #animals #cute #funnyvideo #fy #dogsoftiktok #puppy #poodle",
       song: "Cute Emoji Song",
       likes: 4235000,
       messages: 43000,
@@ -34,21 +37,32 @@ function App() {
   ]);
   return (
     <div className="app">
-      <div className="app_videos">
-        {videos.map(
-          ({autoplay, url, channel, description, song, likes, messages, shares }) => (
-            <Video
-              key={url}
-              url={url}
-              channel={channel}
-              description={description}
-              song={song}
-              likes={likes}
-              messages={messages}
-              shares={shares}
-            />
-          )
-        )}
+      <div className="wrapper">
+        <div className="app_videos">
+          {videos.map(
+            ({
+              autoplay,
+              url,
+              channel,
+              description,
+              song,
+              likes,
+              messages,
+              shares,
+            }) => (
+              <Video
+                key={url}
+                url={url}
+                channel={channel}
+                description={description}
+                song={song}
+                likes={likes}
+                messages={messages}
+                shares={shares}
+              />
+            )
+          )}
+        </div>
       </div>
     </div>
   );
