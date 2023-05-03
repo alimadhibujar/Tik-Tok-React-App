79ba105f282214959a674b8576ce3ce4449a9d92
Bujar Alimadhi <b.alimadhi@gmail.com>
2023-05-03 11:23:25 +0200

    adding instructions info

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,65 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import "./app.css";
 import Video from "../Video/Video";
-import data from "../../assets/data.json";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    // for local fetch json data must be in public folder
-    // fetch('http://localhost:3000/Tik-Tok-React-App/data.json')
-    // .then(response => response.json())
-    // .then(data => setVideos(data));
-    setVideos(data);
-    // instruction info
-    const timer = setTimeout(() => {
-      alert(
-        `Click on the video to play or to pause it, also scroll to see all the videos. Happy watching :)`
-      );
-    }, 1000);
-    return () => clearTimeout(timer);
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch(
+          "https://alimadhibujar.github.io/Tik-Tok-React-App/data.json"
+        );
+        const listVideos = await response.json();
+        setVideos(listVideos);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(`There was an error, ${err}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchVideos();
   }, []);
 
   return (
     <div className="app">
       <div className="wrapper">
-        <div className="app_videos">
-          {videos.map(
-            ({ url, channel, description, song, likes, messages, shares }) => (
-              <Video
-                key={url}
-                url={url}
-                channel={channel}
-                description={description}
-                song={song}
-                likes={likes}
-                messages={messages}
-                shares={shares}
-              />
-            )
-          )}
-        </div>
+        {!isLoading && !fetchError ? (
+          <div className="app_videos">
+            {videos.map(
+              ({
+                url,
+                channel,
+                description,
+                song,
+                likes,
+                messages,
+                shares,
+              }) => (
+                <Video
+                  key={url}
+                  url={url}
+                  channel={channel}
+                  description={description}
+                  song={song}
+                  likes={likes}
+                  messages={messages}
+                  shares={shares}
+                />
+              )
+            )}
+          </div>
+        ) : (
+          <div className="loading">{fetchError || "Loading..."}</div>
+        )}
       </div>
+      <p className="app-info">
+        Click on the video to play or to pause it, also scroll the videos to see
+        them all. Happy watching :)
+      </p>
     </div>
   );
 }
